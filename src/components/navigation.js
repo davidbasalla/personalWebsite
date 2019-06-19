7654de379880517cc9e9b979bb834264d728277a
David Basalla <davidbasalla@gmail.com>
2019-06-19 20:54:59 +0100

    Add Navigation bar placeholders

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,13 +3,26 @@ import { Link } from 'gatsby'
 import styles from './navigation.module.css'
 
 export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
+  <nav role="navigation" className={styles.navigation}>
+    <div className={styles.logo}>
+      <Link to="/">David Basalla [developer | 3D artist]</Link>
+    </div>
+
+    <ul className={styles.navigationList}>
       <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
+        <Link to="/blog/">CODE</Link>
       </li>
+
       <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
+        <Link to="/blog/">MOVIES</Link>
+      </li>
+
+      <li className={styles.navigationItem}>
+        <Link to="/blog/">BLOG</Link>
+      </li>
+
+      <li className={styles.navigationItem}>
+        <Link to="/blog/">ABOUT ME</Link>
       </li>
     </ul>
   </nav>
