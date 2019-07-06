cc83f8f59d4fa4c850b81683fd32bef7ca454111
David Basalla <davidbasalla@gmail.com>
2019-07-06 20:14:56 +0100

    Add nav links for the movie page

--- src/templates/movie.js
+++ src/templates/movie.js
@@ -13,9 +13,18 @@ import imdb from '../../assets/images/imdb.jpg'
 class MovieTemplate extends React.Component {
   render() {
     const movie = get(this.props, 'data.contentfulMovie')
+    const movies = get(this.props, 'data.allContentfulMovie.edges')
+
     const duration = movie.endYear
       ? `${movie.startYear} - ${movie.endYear}`
       : movie.startYear
+
+    const index = movies.findIndex(mov => mov.node.id === movie.id)
+    const previousIndex = (index - 1 + movies.length) % movies.length
+    const nextIndex = (index + 1) % movies.length
+
+    const previousMovie = movies[previousIndex]
+    const nextMovie = movies[nextIndex]
 
     return (
       <Layout location={this.props.location}>
@@ -68,6 +77,14 @@ class MovieTemplate extends React.Component {
                 <ReactMarkdown source={movie.description.description} />
               </div>
             </div>
+            <div className={styles.movieNavigation}>
+              <Link to={`/movies/${previousMovie.node.id}`}>
+                {`< ${previousMovie.node.title}`}
+              </Link>
+              <Link to={`/movies/${nextMovie.node.id}`}>
+                {`${nextMovie.node.title} >`}
+              </Link>
+            </div>
           </div>
         </div>
       </Layout>
@@ -85,6 +102,7 @@ export const pageQuery = graphql`
       }
     }
     contentfulMovie(id: { eq: $id }) {
+      id
       title
       description {
         description
@@ -103,6 +121,14 @@ export const pageQuery = graphql`
         id
         fluid(maxHeight: 600, maxWidth: 400) {
           ...GatsbyContentfulFluid
+        }
+      }
+    }
+    allContentfulMovie(sort: { fields: startYear, order: ASC }) {
+      edges {
+        node {
+          id
+          title
         }
       }
     }
