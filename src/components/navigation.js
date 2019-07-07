5dfb67e0fddf816c892b6492019d148112721359
David Basalla <davidbasalla@gmail.com>
2019-07-07 19:12:06 +0100

    Add hamburger menu on mobile!!

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,53 +1,134 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import HamburgerMenu from 'react-hamburger-menu'
 import styles from './navigation.module.css'
 
-export default () => (
-  <nav role="navigation" className={styles.navigation}>
-    <div className={styles.logo}>
-      <Link to="/">David Basalla</Link>
-    </div>
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props)
 
-    <ul className={styles.navigationList}>
-      <li className={styles.navigationItem}>
-        <Link
-          className={styles.link}
-          to={'/code/'}
-          activeClassName={styles.activeLink}
-        >
-          Code
-        </Link>
-      </li>
+    this.state = {
+      showButtonClicked: false,
+    }
 
-      <li className={styles.navigationItem}>
-        <Link
-          className={styles.link}
-          to={'/movies/'}
-          activeClassName={styles.activeLink}
-        >
-          VFX
-        </Link>
-      </li>
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-      <li className={styles.navigationItem}>
-        <Link
-          className={styles.link}
-          to={'/blog/'}
-          activeClassName={styles.activeLink}
-        >
-          Blog
-        </Link>
-      </li>
+  handleClick() {
+    this.setState({ showButtonClicked: !this.state.showButtonClicked })
+  }
 
-      <li className={styles.navigationItem}>
-        <Link
-          className={styles.link}
-          to={'/about-me/'}
-          activeClassName={styles.activeLink}
+  render() {
+    return (
+      <div>
+        <nav role="navigation" className={styles.navigation}>
+          <div className={styles.logo}>
+            <Link to="/">David Basalla</Link>
+          </div>
+
+          <ul className={styles.navigationList}>
+            <li className={styles.navigationItem}>
+              <Link
+                className={styles.link}
+                to={'/code/'}
+                activeClassName={styles.activeLink}
+              >
+                Code
+              </Link>
+            </li>
+
+            <li className={styles.navigationItem}>
+              <Link
+                className={styles.link}
+                to={'/movies/'}
+                activeClassName={styles.activeLink}
+              >
+                VFX
+              </Link>
+            </li>
+
+            <li className={styles.navigationItem}>
+              <Link
+                className={styles.link}
+                to={'/blog/'}
+                activeClassName={styles.activeLink}
+              >
+                Blog
+              </Link>
+            </li>
+
+            <li className={styles.navigationItem}>
+              <Link
+                className={styles.link}
+                to={'/about-me/'}
+                activeClassName={styles.activeLink}
+              >
+                About
+              </Link>
+            </li>
+          </ul>
+
+          <div className={styles.hamburgerContainer}>
+            <HamburgerMenu
+              isOpen={this.state.showButtonClicked}
+              menuClicked={this.handleClick}
+              width={40}
+              height={20}
+              strokeWidth={3}
+              rotate={0}
+              color="white"
+              borderRadius={0}
+              animationDuration={0.5}
+            />
+          </div>
+        </nav>
+
+        <ul
+          className={`${styles.navigationListMobile} ${this.state
+            .showButtonClicked && styles.showMobileNavBar}`}
         >
-          About
-        </Link>
-      </li>
-    </ul>
-  </nav>
-)
+          <li className={styles.navigationItemMobile}>
+            <Link
+              className={styles.link}
+              to={'/code/'}
+              activeClassName={styles.activeLink}
+            >
+              Code
+            </Link>
+          </li>
+
+          <li className={styles.navigationItemMobile}>
+            <Link
+              className={styles.link}
+              to={'/movies/'}
+              activeClassName={styles.activeLink}
+            >
+              VFX
+            </Link>
+          </li>
+
+          <li className={styles.navigationItemMobile}>
+            <Link
+              className={styles.link}
+              to={'/blog/'}
+              activeClassName={styles.activeLink}
+            >
+              Blog
+            </Link>
+          </li>
+          <li className={`${styles.navigationItemMobile}`}>
+            <Link
+              className={styles.link}
+              to={'/about-me/'}
+              activeClassName={styles.activeLink}
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Navigation
